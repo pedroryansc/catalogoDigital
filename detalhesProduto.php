58dd8cff10ba6cbc8f9252293fb1e5c977133afd
pedroryansc <pedroryangamer@gmail.com>
2024-06-18 13:53:58 -0300

    Fim da implementação do HTML e do CSS (PC)

---
+++ detalhesProduto.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Legend of Zelda: Tears of the Kingdom - ANSON</title>
+    <link rel="shortcut icon" href="fav/favicon.ico">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div id="pagina-container">
+        <?php
+            include "trechos/header.html";
+        ?>
+        <div id="grid-produto">
+            <a href="index.php">
+                <div id="grid-voltar">
+                    <div>
+                        <img src="img/voltar.png" id="imgVoltar">
+                    </div>
+                    <div id="txtVoltar">
+                        <span>Voltar</span>
+                    </div>
+                </div>
+            </a>
+            <img src="img/zeldatotk.jpg" id="imgDetalhes">
+            <div>
+                <div id="divNome">
+                    <span id="nomeProd">The Legend of Zelda: Tears of the Kingdom</span>
+                </div>
+                <span class="plataforma red" id="plataforma">Switch</span>
+                <div id="divPreco">
+                    <sup id="moeda">R$</sup> <span id="preco">309,89</span>
+                </div>
+                <p>
+                    <span id="titleDetalhes">Detalhes do produto</span>
+                </p>
+                <p>
+                    <strong>Data de Lançamento:</strong> <span>12 de maio de 2023</span>
+                </p>
+                <p>
+                    <strong>Desenvolvedora:</strong> <span>Nintendo</span>
+                </p>
+                <p>
+                    <strong>Distribuidora:</strong> <span>Nintendo</span>
+                </p>
+                <p>
+                    <strong>Gênero:</strong> <span>Aventura, Ação, RPG</span>
+                </p>
+            </div>
+        </div>
+        <?php
+            include "trechos/footer.html";
+        ?>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Catálogo digital - ANSON</title>
+    <link rel="shortcut icon" href="fav/favicon.ico">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div id="pagina-container">
+        <?php
+            include "trechos/header.html";
+        ?>
+        <section id="slideshow">
+            <img src="img/promocao.png" id="promocao">
+            <img src="img/seta.png" id="setaEsquerda">
+            <img src="img/seta.png" id="setaDireita">
+        </section>
+        <div id="grid-container">
+            <div class="grid-item">
+                <a href="detalhesProduto.php">
+                    <img src="img/zeldatotk.jpg" class="imgProduto"/>
+                </a>
+                <div class="divNome">
+                    <a href="detalhesProduto.php">
+                        <span class="nomeProd">The Legend of Zelda: Tears of the Kingdom</span>
+                    </a>
+                </div>
+                <a href="detalhesProduto.php">
+                    <span class="plataforma red">Switch</span>
+                </a>
+                <a href="detalhesProduto.php">
+                    <div class="divPreco">
+                        <sup class="moeda">R$</sup> <span class="preco">309,89</span>
+                    </div>
+                </a>
+            </div>
+            <div class="grid-item">
+                <img src="img/eldenring.jpg" class="imgProduto"/>
+                <div class="divNome">
+                    <span class="nomeProd">Elden Ring</span>
+                </div>
+                <span class="plataforma green">Xbox Series X / Xbox One</span>
+                <div class="divPreco">
+                    <sup class="moeda">R$</sup> <span class="preco">366,87</span>
+                </div>
+            </div>
+        </div>
+        <?php
+            include "trechos/footer.html";
+        ?>
+    </div>
+</body>
+</html>
