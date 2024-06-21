c458840756994871d8288eef8b4331c31340daa6
pedroryansc <pedroryangamer@gmail.com>
2024-06-20 23:30:32 -0300

    Início da implementação do CSS para celular

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Catálogo digital - ANSON</title>
     <link rel="shortcut icon" href="fav/favicon.ico">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/celular.css">
 </head>
 <body>
     <div id="pagina-container">
@@ -19,31 +19,43 @@
         </section>
         <div id="grid-container">
             <div class="grid-item">
-                <a href="detalhesProduto.php">
-                    <img src="img/zeldatotk.jpg" class="imgProduto"/>
-                </a>
-                <div class="divNome">
-                    <a href="detalhesProduto.php">
-                        <span class="nomeProd">The Legend of Zelda: Tears of the Kingdom</span>
-                    </a>
-                </div>
-                <a href="detalhesProduto.php">
-                    <span class="plataforma red">Switch</span>
-                </a>
-                <a href="detalhesProduto.php">
-                    <div class="divPreco">
-                        <sup class="moeda">R$</sup> <span class="preco">309,89</span>
+                <div class="grid-produto">
+                    <div>
+                        <a href="detalhesProduto.php">
+                            <img src="img/zeldatotk.jpg" class="imgProduto"/>
+                        </a>
                     </div>
-                </a>
+                    <div>
+                        <div class="divNome">
+                            <a href="detalhesProduto.php">
+                                <span class="nomeProd">The Legend of Zelda: Tears of the Kingdom</span>
+                            </a>
+                        </div>
+                        <a href="detalhesProduto.php">
+                            <span class="plataforma red">Switch</span>
+                        </a>
+                        <a href="detalhesProduto.php">
+                            <div class="divPreco">
+                                <sup class="moeda">R$</sup> <span class="preco">309,89</span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
             </div>
             <div class="grid-item">
-                <img src="img/eldenring.jpg" class="imgProduto"/>
-                <div class="divNome">
-                    <span class="nomeProd">Elden Ring</span>
-                </div>
-                <span class="plataforma green">Xbox Series X / Xbox One</span>
-                <div class="divPreco">
-                    <sup class="moeda">R$</sup> <span class="preco">366,87</span>
+                <div class="grid-produto">
+                    <div>
+                        <img src="img/eldenring.jpg" class="imgProduto"/>
+                    </div>
+                    <div>
+                        <div class="divNome">
+                            <span class="nomeProd">Elden Ring</span>
+                        </div>
+                        <span class="plataforma green">Xbox Series X / Xbox One</span>
+                        <div class="divPreco">
+                            <sup class="moeda">R$</sup> <span class="preco">366,87</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
